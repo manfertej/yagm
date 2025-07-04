a5b6b7e9cfddb030f60538980ce2083290211bde
Manuel <manfertej@gmail.com>
2025-07-05 00:47:57 +0200

    Initial commit

---
+++ src/main/java/dev/manfertej/yagm/YagmApplication.java
@@ -0,0 +1,13 @@
+package dev.manfertej.yagm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagmApplication.class, args);
+	}
+
+}
